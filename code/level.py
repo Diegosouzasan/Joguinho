336e89a63310094765661474361d40e093752c10
Diego Souza <diegosouza.ne@gmail.com>
2025-07-01 22:45:16 -0300

    Addinng classes genereted from StarUML

---
+++ code/level.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Level:
+    def __init__(self):
+        self.window = None
+        self.name str = None
+        self.entiy_list = None
+
+    def run(self, ):
+        pass
