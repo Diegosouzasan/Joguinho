58d5b9d46188f7cc51df3f575ac9a6932bd74a25
Diego Souza <diegosouza.ne@gmail.com>
2025-07-03 20:48:12 -0300

    Creating Menu interations Creating ABC class Creating Factory Level 1 bg implementation: Parallax

--- code/Const.py
+++ code/Const.py
@@ -4,12 +4,25 @@ from pygame.examples.grid import WINDOW_WIDTH
 COLOR_WHITE =  (240, 251, 248, 98)
 COLOR_BLUE = (231, 249, 251, 98)
 
+#y
+COLOR_YELLOW = (252, 230, 40, 99)
+
 #M
 MENU_OPTION = ('NEW GAME 1P',
                'NEW GAME 2P - COOPERATIVE',
                'NEW GAME 2P- COMPETITIVE',
                'SCORE',
                'EXIT')
+
+#e
+ENTITY_SPEED = {
+    'Level1bg0': 0,
+    'Level1bg1': 1,
+    'Level1bg2':2,
+    'Level1bg3':3,
+    'Level1bg4':4,
+}
+
 
 #w
 WIN_WIDTH = 634
--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from abc import abstractmethod, ABC
 
-class EntityFactory:
-    def __init__(self):
-        pass
+from pygame.examples.video import backgrounds
 
-    def get_entity(self, entity_type):
-        pass
+from code.Background import Background
+from code.Const import WIN_WIDTH
+
+
+class EntityFactory(ABC):
+
+    @abstractmethod
+    def get_entity(self: str, position=(0, 0)):
+        match self:
+            case 'Level1bg':
+                list_bg = []
+                for i in range(4):
+                    list_bg.append(Background(f'Level1bg{i}', (0, 0)))
+                    list_bg.append(Background(f'Level1bg{i}', (WIN_WIDTH, 0)))
+                return list_bg
+        return None
+
+
